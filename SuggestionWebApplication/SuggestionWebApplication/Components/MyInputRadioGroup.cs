0cc7eee7a680dcacf92ffb021ece36d9c73707e2
yangryu321 <yangryu321@gmail.com>
2022-03-18 17:24:00 -0400

    created a custom component called MyInputRadioGroup

--- SuggestionWebApplication/SuggestionWebApplication/Components/MyInputRadioGroup.cs
+++ SuggestionWebApplication/SuggestionWebApplication/Components/MyInputRadioGroup.cs
@@ -8,6 +8,25 @@ namespace SuggestionWebApplication.Components
     public class MyInputRadioGroup<TValue>:InputRadioGroup<TValue>
     {
 
+        private string _name;
+        private string _fieldClass;
+
+        protected override void OnParametersSet()
+        {
+
+            var fieldClass = EditContext?.FieldCssClass(FieldIdentifier) ?? string.Empty;
+
+            if(fieldClass!=_fieldClass||Name!=_name)
+            {
+                _fieldClass = fieldClass;
+                _name = Name;
+
+                base.OnParametersSet();
+
+            }
+
+            
+        }
 
     }
 }
