b9dc90dafdd6315183bd51b8f7535458b01c0a02
yangryu321 <yangryu321@gmail.com>
2022-03-22 21:51:29 -0400

    -Created a new folder called helper
    -Created a new static class called AuthenticationStateProviderHelper
     with an extention method in there called GetUseAuth(
     this AuthenticationStateProvider authProvider, IUserData userData)
    
    -Added a new method called LoadAndVerifyUser() in Index page
     Logic here:
     When you load this page it's gonna run LoadAndVerifyUser()
     It asks Microsoft Azure ADB2C if there is an objectIdentifier for this current
     state(user)
     If there is, it means there is a user signed in
     If not then it's tourist and do nothing
    
     If there is a user signed in, it asks for the datebase if the user is in the
     database by searching for identifier
     If it's not in database then create a new user model
    
     It then asks Microsoft for other information like names, then it compare the info
     with the user from database or the user just created.
     if there is anything changed, then store the new user or update the user in
     database
     if it's not in database(with no id from MongoDb then Create)
     if it's already in database( with id then update)
     ps. cant update Mongo Id with update()

---
+++ SuggestionWebApplication/SuggestionWebApplication/Helpers/AuthenticationStateProviderHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Authorization;
+using SuggestionAppLibrary.DataAccess;
+using SuggestionAppLibrary.Models;
+
+namespace SuggestionWebApplication.Helpers
+{
+    public static class AuthenticationStateProviderHelper
+    {
+        public static async Task<UserModel> GetUserFromAuth(
+            this AuthenticationStateProvider authProvider, IUserData userData)
+        {
+            var authState = await authProvider.GetAuthenticationStateAsync();
+            string objectId = authState.User.Claims.FirstOrDefault(x => x.Type.Contains("objectidentifier"))?.Value;
+            return await userData.GetUserFromAuthentication(objectId);
+
+        }
+    }
+}
